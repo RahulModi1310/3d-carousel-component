7d766e7219733bad277fe617b3cb76c175bd5285
Rahul Modi <rahul_km@ee.iitr.ac.in>
2022-02-21 20:28:45 +0530

    Fixed Major Bugs and Added Demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,41 +1,33 @@
 import React from "react";
 import "./App.css";
 import Carousel3D from "../../src/components/Carousel3D";
+import CardList from "./CarouselList";
 
 function App() {
-  const CardList = [
-    {
-      element: <div>Card 1</div>,
-    },
-    {
-      element: <div>Card 2</div>,
-    },
-    {
-      element: <div>Card 3</div>,
-    },
-    {
-      element: <div>Card 4</div>,
-    },
-    {
-      element: <div>Card 5</div>,
-    },
-    {
-      element: <div>Card 6</div>,
-    },
-    {
-      element: <div>Card 7</div>,
-    },
-    {
-      element: <div>Card 8</div>,
-    },
-  ];
+  const ContainerStyle = {
+    Width: "100%",
+    Height: "max-content",
+    Padding: "2em 5%",
+    BackgroundColor: "#D65A31",
+  };
+
+  const CardStyle = {
+    Width: "100%",
+    Height: "fit-content",
+    AspectRatio: "5/3",
+    Padding: "0.25em",
+    BackgroundColor: "#393E46",
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        3d-carousel-component
-        <Carousel3D ContainerStyle={{}} CardStyle={{}} CardList={CardList} />
-      </header>
+      <h1>3d-carousel-component</h1>
+      <Carousel3D
+        ContainerStyle={ContainerStyle}
+        CardStyle={CardStyle}
+        CardList={CardList}
+      />
+      <h2>💕Hello! Kitten Lovers💕</h2>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  template: path.join(__dirname, "src/demo/public/index.html"),
+  template: path.join(__dirname, "demo/public/index.html"),
   filename: "./index.html",
 });
 module.exports = {
-  entry: path.join(__dirname, "src/demo/src/index.js"),
+  entry: path.join(__dirname, "demo/src/index.js"),
   module: {
     rules: [
       {
